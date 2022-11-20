0dda67195ea2650f842c60dda15e0f0886996b08
Paul Costin <Paul@Vivobook.local>
2022-11-20 14:14:27 +0200

    ipad media query done 80% + hamburger menu

--- app.js
+++ app.js
@@ -12,17 +12,62 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
 
-
+const input = document.querySelectorAll("#mode");
 $(document).ready(function () {
-    $('#mode').click(function () {
-        if ($('link#styles').attr('href') == "Night.css") {
-            $('link#styles').attr('href', 'Day.css')
-            $('#mode').attr('value', 'Toggle Night Mode')
-        }
-        else {
-            $('link#styles').attr('href', 'Night.css')
-            $('#mode').attr('value', 'Toggle Day Mode')
-        }
+    input.forEach(function (handleClick) {
+        handleClick.addEventListener("click", function () {
+            if ($('link#styles').attr('href') == "Night.css") {
+                $('link#styles').attr('href', 'Day.css')
+                $('#mode').attr('value', 'Toggle Night Mode')
+            }
+            else {
+                $('link#styles').attr('href', 'Night.css')
+                $('#mode').attr('value', 'Toggle Day Mode')
+            };
+        })
     })
+})
 
-});
+
+const hamburger = document.querySelectorAll(".material-symbols-outlined");
+hamburger.forEach(function (openMenu) {
+    openMenu.addEventListener("click", function () {
+        document.querySelector('#contact-menubar').classList.toggle("visible");
+        document.body.classList.toggle("fixed");
+    })
+})
+
+function myFunction(section) {
+    if (section == "aboutMe") {
+        document.body.classList.remove("fixed");
+        document.querySelector('#contact-menubar').classList.toggle("visible");
+        document.getElementById('aboutMeSection').scrollIntoView();
+    } else if (section == "projects") {
+        document.body.classList.remove("fixed");
+        document.querySelector('#contact-menubar').classList.toggle("visible");
+        document.getElementById('portofolio').scrollIntoView();
+    } else {
+        document.body.classList.remove("fixed");
+        document.querySelector('#contact-menubar').classList.toggle("visible");
+        document.getElementById('contact-form').scrollIntoView();
+    }
+}
+
+
+
+
+
+
+// const input = $("#mode")
+// $(document).ready(function () {
+//     $('#mode').forEach(input).click(function () {
+//         if ($('link#styles').attr('href') == "Night.css") {
+//             $('link#styles').attr('href', 'Day.css')
+//             $('#mode').attr('value', 'Toggle Night Mode')
+//         }
+//         else {
+//             $('link#styles').attr('href', 'Night.css')
+//             $('#mode').attr('value', 'Toggle Day Mode')
+//         }
+//     })
+// })
